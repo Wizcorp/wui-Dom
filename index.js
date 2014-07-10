b9de94f2ef755b66719d9c8bd9fe6a28916c38a5
Cedric Stoquer <cstoquer@wizcorp.jp>
2014-07-10 12:45:15 +0900

    bugfix: getClassName function fail if rootElement is not defined

--- index.js
+++ index.js
@@ -606,7 +606,7 @@ function removeClassNames(baseList, args) {
  */
 WuiDom.prototype.getClassNames = function () {
 	// returns an array of all class names
-
+	if (!this.rootElement) return [];
 	return parseClassNames(this.rootElement.className);
 };
 
